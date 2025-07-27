6f248d3158080d0658d95132807c591621210bc2
Hannah Holzheu <hh062@hdm-stuttgart.de>
2025-07-27 20:37:25 +0200

    update cli

--- src/nlp/cli.clj
+++ src/nlp/cli.clj
@@ -14,15 +14,19 @@
 
 (def cli-options
   [["-h" "--help" "Show help"]
-   ["-t" "--tokenize [text]" "Tokenize the input text"]
-   ["-p" "--preprocess [text]" "Preprocess the input text"]
-   ["-v" "--vectorize" "Vectorize the input text using the specified function (e.g., tf-idf, one-hot, count-vector)"]
-   ["-d" "--distance [function] [text1] [text2]" "Calculate the distance between two texts using the specified function (e.g., levenshtein, manhattan, euclidean, hamming, chebyshev, minkowski, canberra, normalized-levenshtein)"]
-   ["-s" "--similarity [function] [text1] [text2]" "Calculate the similarity between two texts using the specified function (e.g., cosine, jaccard, sorensen-dice, overlap-coefficient, dice-coefficient)"]
-   ["-cla" "--classify [model] [text]" "Classify the input text using the specified model"]
-   ["-clu" "--cluster [k] [iterations] [texts]" "Cluster the input texts using k-means"]
-   ["-sp" "--spellcheck [word] [dictionary]" "Spellcheck the input word against a dictionary"]
-   ["-su" "--summarize [n] [text]" "Summarize the input text into n sentences"]])
+   ["-t" "--tokenize" "Tokenize the input text"]
+   ["-p" "--preprocess" "Preprocess the input text"]
+   ["-v" "--vectorize" "Vectorize the input text"]
+   ["-d" "--distance" "Calculate distance"]
+   ["-s" "--similarity" "Calculate similarity"]
+   ["-cla" "--classify" "Classify text"]
+   ["-clu" "--cluster" "Cluster texts"]
+   ["-sp" "--spellcheck" "Spellcheck word"]
+   ["-su" "--summarize" "Summarize text"]])
+
+(defn print-error [msg]
+  (binding [*out* *err*]
+    (println "Error:" msg)))
 
 (defn -main [& args]
   (let [{:keys [options arguments errors summary]} (cli/parse-opts args cli-options)]
@@ -31,76 +35,124 @@
       (println summary)
 
       (:tokenize options)
-      (println (tokenize/tokenize (:tokenize options)))
+      (if (empty? arguments)
+        (print-error "Tokenize requires one text argument.")
+        (println (tokenize/tokenize (first arguments))))
 
       (:preprocess options)
-      (println (preprocess/preprocess (:preprocess options)))
+      (if (empty? arguments)
+        (print-error "Preprocess requires one text argument.")
+        (println (preprocess/preprocess (first arguments))))
 
       (:vectorize options)
-      (let [[func text] arguments]
-        (println "func:" func)
-        (println "text:" text)
-        (case func
-          "tf-idf" (println ((vectorize/tf-idf [(tokenize/tokenize text)]) (tokenize/tokenize text)))
-          "one-hot" (println (vectorize/one-hot (tokenize/tokenize text)))
-          "count-vector" (println (vectorize/count-vector (tokenize/tokenize text)))))
+      (if (< (count arguments) 2)
+        (print-error "Vectorize requires a function name and a text.")
+        (let [[func text] arguments]
+          (case func
+            "tf-idf" (println ((vectorize/tf-idf [(tokenize/tokenize text)]) (tokenize/tokenize text)))
+            "one-hot" (println (vectorize/one-hot (tokenize/tokenize text)))
+            "count-vector" (println (vectorize/count-vector (tokenize/tokenize text)))
+            (print-error (str "Unknown vectorize function: " func)))))
 
       (:distance options)
-      (let [[func text1 text2] arguments]
-        (case func
-          "levenshtein" (println (distance/levenshtein text1 text2))
-          "manhattan" (println (distance/manhattan-distance
-                                (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
-                                (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
-          "euclidean" (println (distance/euclidean-distance
-                                (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
-                                (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
-          "hamming" (println (distance/hamming-distance text1 text2))
-          "chebyshev" (println (distance/chebyshev-distance
-                                (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
-                                (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
-          "minkowski" (println (distance/minkowski-distance
-                                (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
-                                (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))
-                                (read-string (last arguments))))
-          "canberra" (println (distance/canberra-distance
-                               (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
-                               (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
-          "normalized-levenshtein" (println (distance/normalized-levenshtein text1 text2))))
+      (if (< (count arguments) 3)
+        (print-error "Distance requires a function name and two texts.")
+        (let [[func text1 text2 & more] arguments]
+          (case func
+            "levenshtein" (println (distance/levenshtein text1 text2))
+            "manhattan" (println (distance/manhattan-distance
+                                  (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
+                                  (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
+            "euclidean" (println (distance/euclidean-distance
+                                  (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
+                                  (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
+            "hamming" (println (distance/hamming-distance text1 text2))
+            "chebyshev" (println (distance/chebyshev-distance
+                                  (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
+                                  (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
+            "minkowski" (if (empty? more)
+                          (print-error "Minkowski distance requires an order parameter as well.")
+                          (println (distance/minkowski-distance
+                                    (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
+                                    (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))
+                                    (read-string (first more)))))
+            "canberra" (println (distance/canberra-distance
+                                 (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
+                                 (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
+            "normalized-levenshtein" (println (distance/normalized-levenshtein text1 text2))
+            (print-error (str "Unknown distance function: " func)))))
 
       (:similarity options)
-      (let [[func text1 text2] arguments]
-        (case func
-          "cosine" (println (similarity/cosine-sim
-                             (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
-                             (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
-          "jaccard" (println (similarity/jaccard (tokenize/tokenize text1) (tokenize/tokenize text2)))
-          "sorensen-dice" (println (similarity/sorensen-dice (tokenize/tokenize text1) (tokenize/tokenize text2)))
-          "overlap-coefficient" (println (similarity/overlap-coefficient (tokenize/tokenize text1) (tokenize/tokenize text2)))
-          "dice-coefficient" (println (similarity/dice-coefficient
-                                       (vectorize/term-frequency (tokenize/tokenize text1))
-                                       (vectorize/term-frequency (tokenize/tokenize text2))))))
+      (if (< (count arguments) 3)
+        (print-error "Similarity requires a function name and two texts.")
+        (let [[func text1 text2] arguments]
+          (case func
+            "cosine" (println (similarity/cosine-sim
+                               (first (vals ((vectorize/tf-idf [(tokenize/tokenize text1)]) (tokenize/tokenize text1))))
+                               (first (vals ((vectorize/tf-idf [(tokenize/tokenize text2)]) (tokenize/tokenize text2))))))
+            "jaccard" (println (similarity/jaccard (tokenize/tokenize text1) (tokenize/tokenize text2)))
+            "sorensen-dice" (println (similarity/sorensen-dice (tokenize/tokenize text1) (tokenize/tokenize text2)))
+            "overlap-coefficient" (println (similarity/overlap-coefficient (tokenize/tokenize text1) (tokenize/tokenize text2)))
+            "dice-coefficient" (println (similarity/dice-coefficient
+                                         (vectorize/term-frequency (tokenize/tokenize text1))
+                                         (vectorize/term-frequency (tokenize/tokenize text2))))
+            (print-error (str "Unknown similarity function: " func)))))
 
       (:classify options)
-      (let [[model-str text] arguments
-            model (read-string model-str)]
-        (println (classification/classify model (tokenize/tokenize text))))
+      (if (< (count arguments) 2)
+        (print-error "Classify requires a model and a text.")
+        (let [[model-str text & more] arguments
+              model (try
+                      (read-string model-str)
+                      (catch Exception _
+                        (print-error "Invalid model format. Please provide a valid Clojure data structure.")
+                        nil))]
+          (when model
+            (println (classification/classify model (tokenize/tokenize text))))))
 
       (:cluster options)
-      (let [[k-str iterations-str & texts] arguments
-            k (read-string k-str)
-            iterations (read-string iterations-str)
-            tokenized-texts (map tokenize/tokenize texts)
-            vectorizer (vectorize/tf-idf tokenized-texts)
-            vectors (map vectorizer tokenized-texts)]
-        (println (clustering/k-means vectors k iterations)))
+      (if (< (count arguments) 3)
+        (print-error "Cluster requires k, iterations, and at least one text.")
+        (let [[k-str iterations-str & texts] arguments
+              k (try
+                  (read-string k-str)
+                  (catch Exception _
+                    (print-error "Invalid k parameter. Should be a number.")
+                    nil))
+              iterations (try
+                           (read-string iterations-str)
+                           (catch Exception _
+                             (print-error "Invalid iterations parameter. Should be a number.")
+                             nil))]
+          (when (and k iterations)
+            (let [tokenized-texts (map tokenize/tokenize texts)
+                  vectorizer (vectorize/tf-idf tokenized-texts)
+                  vectors (map vectorizer tokenized-texts)]
+              (println (clustering/k-means vectors k iterations))))))
 
       (:spellcheck options)
-      (let [[word & dictionary] arguments]
-        (println (spellcheck/correct-word word dictionary)))
+      (if (empty? arguments)
+        (print-error "Spellcheck requires a word and optionally a dictionary.")
+        (let [[word & dictionary] arguments]
+          (println (spellcheck/correct-word word dictionary))))
 
       (:summarize options)
-      (let [[n-str & text-parts] arguments
-            n (read-string n-str)
-            text (str/join " " text-parts)]
-        (println (summarize/extractive-summary text n))))))
+      (if (< (count arguments) 2)
+        (print-error "Summarize requires a number of sentences and a text.")
+        (let [[n-str & text-parts] arguments
+              n (try
+                  (read-string n-str)
+                  (catch Exception _
+                    (print-error "Invalid number for sentences.")
+                    nil))
+              text (str/join " " text-parts)]
+          (when n
+            (println (summarize/extractive-summary text n)))))
+
+      (not (empty? errors))
+      (do
+        (doseq [e errors] (print-error e))
+        (println summary))
+
+      :else
+      (print-error "Keine gültige Option angegeben. Benutze --help für Hilfe."))))
